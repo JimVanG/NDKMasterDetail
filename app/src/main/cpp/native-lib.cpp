5205b3487694bdd7f4b8255402617ffbb2fa388f
JimVanG <jjvg@me.com>
2019-01-18 09:17:15 -0600

    Use product flavors to reduce bloat by only including the device's specific CPU architecture's ABI.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_jimvang_ndkmasterdetail_MainActivity_stringFromJNI(
+Java_com_jimvang_ndkmasterdetail_ItemListActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
--- app/src/main/java/com/jimvang/ndkmasterdetail/ItemListActivity.java
+++ app/src/main/java/com/jimvang/ndkmasterdetail/ItemListActivity.java
@@ -58,7 +58,7 @@ public class ItemListActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, stringFromJNI(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
